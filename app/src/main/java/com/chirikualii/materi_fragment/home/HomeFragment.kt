ae0c4ec107292a80ccc164353c723dd3e6a9eb51
Lab-ERP-29 <habijiyan111@gmail.com>
2022-10-29 15:00:32 +0700

    first commit

---
+++ app/src/main/java/com/chirikualii/materi_fragment/home/HomeFragment.kt
@@ -0,0 +1,43 @@
+package com.chirikualii.materi_fragment.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chirikualii.materi_fragment.R
+import com.chirikualii.materi_fragment.databinding.FragmentHomeBinding
+import com.chirikualii.materi_fragment.home.nowplaying.NowPlayingFragment
+import com.chirikualii.materi_fragment.home.popular.PopularFragment
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    private val listFragment = listOf(
+        NowPlayingFragment(),
+        PopularFragment()
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(
+            inflater, container, false
+        )
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val viewPagerAdapter = ViewPagerAdapter(
+            listFragment, parentFragmentManager
+        )
+
+        binding.viewPager.adapter = viewPagerAdapter
+
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
+    }
+}
